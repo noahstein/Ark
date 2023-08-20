2c0691131b9c0d79228e015fe79ed2924e3be3b2
Noah Stein <noah@fourthmonkeygames.com>
2023-08-19 22:21:28 -0700

    Refactor CMake files for multiplatform projects.
    macOS Arm64 functional in new system. Windows is broken.
    Add top-level cmake directory for additional CMake files.
    Add cmake/toolchains for future toolchain files.
    Add cmake/toolchains/settings for settings.

---
+++ Ark/cmake/toolchains/settings/macOS/macOS-arm64.cmake
@@ -0,0 +1,8 @@
+#*******************************************************************************
+# Setup code common to host and toolchain builds for Arm64 MacOS builds.
+#
+# Copyright © 2023 Noah Stein. All Rights Reserved.
+#*******************************************************************************
+
+set(CMAKE_OSX_ARCHITECTURES "arm64" CACHE STRING "" FORCE)
+set(HAL_SIMD Neon32)
