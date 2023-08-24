3e1f28622c41637d58c043758df3548bf3b18310
Noah Stein <noah@fourthmonkeygames.com>
2023-08-24 15:02:02 -0700

    Fix macOS build breakage.
    The default to arm64 had been erroneously been moved somewhere
    irrelevant, actually after it was already necessary.

--- Ark/cmake/toolchains/settings/Darwin.cmake
+++ Ark/cmake/toolchains/settings/Darwin.cmake
@@ -1,11 +1,15 @@
-#*******************************************************************************
-# CMake file for processing settings for macOS builds. This file is to be 
-# included when building on a Mac without specifying a specific toolchain file 
-# to use. Most of the settings are handled in included files. This file is 
-# responsible for evaluating host information and then relying upon setup files 
+# *******************************************************************************
+# CMake file for processing settings for macOS builds. This file is to be
+# included when building on a Mac without specifying a specific toolchain file
+# to use. Most of the settings are handled in included files. This file is
+# responsible for evaluating host information and then relying upon setup files
 # common with toolchain builds.
 #
 # Copyright © 2023 Noah Stein. All Rights Reserved.
-#*******************************************************************************
+# *******************************************************************************
 
+# Default macOS to arm64
+set(CMAKE_OSX_ARCHITECTURES "arm64" CACHE STRING "" FORCE)
+
+# Include configuration based on CPU processor
 include("cmake/toolchains/settings/macOS/macOS-${CMAKE_OSX_ARCHITECTURES}.cmake")
